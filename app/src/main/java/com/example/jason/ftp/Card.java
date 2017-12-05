dee3490a30b208e0b6f24118d9fc92d14a2b973c
Jacob Young <jwyoung07@gmail.com>
2017-12-05 02:23:53 -0800

    Comments

--- app/src/main/java/com/example/jason/ftp/Card.java
+++ app/src/main/java/com/example/jason/ftp/Card.java
@@ -1,21 +1,30 @@
+/***
+ * file: HSActivity.java
+ * author: Team FTP
+ * class: CS 245 - Programming Graphical User Interfaces
+ *
+ * assignment: Android Studio Project
+ * date last modified: 12/5/17
+ *
+ * purpose: This is the basic class that allows us to manage the cards during the memory game
+ *
+ **/
+
 package com.example.jason.ftp;
 
 import android.widget.Button;
 
 import java.io.Serializable;
 
-
 public class Card implements Serializable{
 
 	public int x;
 	public int y;
-	public Button button;
+	public Button button; 	//used to select the card on screen
 	
-	public Card(Button button, int x,int y) {
+	public Card(Button button, int x,int y) { //each card has a button and x and y coords
 		this.x = x;
 		this.y=y;
 		this.button=button;
 	}
-	
-
 }
